26dafb8156f648fbf83c31ae3cd783d27d337ee6
TriLinder <82846064+TriLinder@users.noreply.github.com>
2022-01-22 22:15:59 +0100

    Hide Flask server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_file
 from addFile import getPort
 import shelve
+import ctypes
 import os
 
 app = Flask(__name__)
@@ -22,4 +23,6 @@ def getFile(id) :
     
     return send_file(path)
 
-app.run(threaded=True, host="0.0.0.0", port=getPort())
+if __name__ == "__main__" :
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+    app.run(threaded=True, host="0.0.0.0", port=getPort())
